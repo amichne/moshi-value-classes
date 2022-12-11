a2deb4d98fb316ef9e884e62e8f4bd9ac11b5e7b
Austin Michne <austinmichne@gmail.com>
2022-12-11 01:02:08 -0500

    It works yay

--- adapter/src/main/kotlin/io/amichne/moshi/extension/UnsignedAdapterFactory.kt
+++ adapter/src/main/kotlin/io/amichne/moshi/extension/UnsignedAdapterFactory.kt
@@ -3,67 +3,61 @@ package io.amichne.moshi.extension
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.JsonDataException
 import com.squareup.moshi.JsonReader
+import com.squareup.moshi.JsonReader.Token
 import com.squareup.moshi.JsonWriter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.rawType
 import java.lang.reflect.Type
-import kotlin.math.sign
 
-private class UnsignedTypeAdapter<UnsignedT, SignedT : Number>(
-  private val typeMapper: TypeMapper<UnsignedT, SignedT>,
+private class UnsignedTypeAdapter<UnsignedT>(
+  private val numberAdapter: NumberAdapter<UnsignedT>,
 ) : JsonAdapter<UnsignedT>() {
   override fun toJson(writer: JsonWriter, value: UnsignedT?) {
     when (value) {
       null -> writer.nullValue()
-      else -> writer.value(typeMapper.unsignedToSigned(value))
+      else -> writer.valueSink().use { it.writeUtf8(value.toString()) }
     }
   }
 
-  override fun fromJson(reader: JsonReader): UnsignedT? = reader.readJsonValue()?.let { value ->
-    when (value) {
-      is Double -> {
-        if (!value.rem(1).equals(0.0)) {
-          // TODO - throw a real JsonDataException here
-          error("non-integer value")
-        } else if (value.sign < 0) {
-          // TODO - throw a real JsonDataException here
-          error("negative value")
-        } else {
-          typeMapper.signedToUnsigned(value.toLong())
-        }
+  override fun fromJson(reader: JsonReader): UnsignedT? = when (val next = reader.peek()) {
+    Token.NUMBER -> {
+      val stringOfNumber = reader.nextString()
+      if (stringOfNumber.startsWith('-')) {
+        error("negative value")
       }
-
-      else -> throw JsonDataException(
-        "Expected an unsigned number but was $value, a ${value::class.java.simpleName}, at path ${reader.path}"
-      )
+      numberAdapter.unsignedParser(stringOfNumber.toULong())
     }
+
+    else -> throw JsonDataException(
+      "Expected an unsigned number but was ${reader.readJsonValue()}, " +
+      "a $next, at path ${reader.path}"
+    )
   }
 }
 
-private class TypeMapper<UnsignedT, SignedT : Number> private constructor(
-  val unsignedToSigned: UnsignedT.() -> Long,
-  val signedToUnsigned: Long.() -> UnsignedT,
+private class NumberAdapter<UnsignedT> private constructor(
+  val unsignedParser: ULong.() -> UnsignedT,
 ) {
   companion object {
-    val Int = TypeMapper<UInt, Int>({ toLong() }, { toUInt() })
-    val Long = TypeMapper<ULong, Long>({ toLong() }, { toULong() })
-    val Short = TypeMapper<UShort, Short>({ toLong() }, { toUShort() })
-    val Byte = TypeMapper<UByte, Byte>({ toLong() }, { toUByte() })
+    val Int = NumberAdapter { toUInt() }
+    val Long = NumberAdapter { toULong() }
+    val Short = NumberAdapter { toUShort() }
+    val Byte = NumberAdapter { toUByte() }
   }
 }
 
 object UnsignedAdapterFactory : JsonAdapter.Factory {
-  private val unsignedTypesMapperMap: Map<Class<*>, TypeMapper<*, *>> = mapOf(
-    ULong::class.java to TypeMapper.Long,
-    UInt::class.java to TypeMapper.Int,
-    UShort::class.java to TypeMapper.Short,
-    UByte::class.java to TypeMapper.Byte
+  private val unsignedTypesMapperMap: Map<Class<*>, NumberAdapter<*>> = mapOf(
+    ULong::class.java to NumberAdapter.Long,
+    UInt::class.java to NumberAdapter.Int,
+    UShort::class.java to NumberAdapter.Short,
+    UByte::class.java to NumberAdapter.Byte
   )
 
   private val Type.isUnsignedType: Boolean
     get() = unsignedTypesMapperMap.keys.any { it.isAssignableFrom(rawType) }
 
-  private val Type.mapper: TypeMapper<*, *>
+  private val Type.mapper: NumberAdapter<*>
     get() = unsignedTypesMapperMap[rawType]!!
 
   override fun create(
